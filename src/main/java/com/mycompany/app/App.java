7732d877eb6d943b124008a71dab242b45936061
Ren Yi Lee <jerrylee99083010@gmail.com>
2023-02-27 07:59:31 +0000

    Updated string to trigger SCM Change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("This is a dummy string");
+        System.out.println("This string is updated to trigger SCM Change");
     }
 
     public String getMessage() {
