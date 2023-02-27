1d44e49ab3e57c1f14d6266c7bd44baa05345d25
Ren Yi Lee <jerrylee99083010@gmail.com>
2023-02-27 08:42:58 +0000

    String changed to trigger webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("This string is updated to trigger SCM Change");
+        System.out.println("This string is updated to trigger webhook");
     }
 
     public String getMessage() {
