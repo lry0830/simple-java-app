a9c7408ad5c922fdad411902d4f1ecbc2246ba72
Ren Yi Lee <jerrylee99083010@gmail.com>
2023-02-28 05:44:59 +0000

    Commit to check for Github Webhook

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("This string is updated to trigger SCM through pipeline");
+        System.out.println("This string is updated to trigger SCM through pipeline!");
     }
 
     public String getMessage() {
